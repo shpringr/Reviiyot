29f547d1c5dbad2db1f3c65b3fbff0338b828cc1
shpringr <shpringr@post.bgu.ac.il>
2016-11-21 23:12:28 +0200

    nvnfdjvn

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,50 @@
+#include <string>
+#include <vector>
 #include <iostream>
 #include "Card.h"
 #include "Game.h"
-
 using namespace std;
 
 int main(int argc, char **argv) {
+
+	FigureCard * C = new FigureCard('A','C');
+	NumericCard* C1 = new NumericCard('3','C');
+
+	vector<Card *> myVector1;
+	myVector1.push_back(C);
+	myVector1.push_back(C1);
+
+
+
+	for(unsigned int i =0; i<myVector1.size(); i++){
+		cout << myVector1.size() << "  " << myVector1[i] <<  " ";
+	}
+
+	cout << myVector1[0]->toString() << "  " ;
+
+	vector<int> myVector;
+	myVector.push_back(3);
+	myVector.push_back(4);
+	myVector.push_back(5);
+	myVector.push_back(6);
+
+	cout << "myVector:" << endl;
+
+	for(unsigned int i =0; i<myVector.size(); i++){
+		cout << myVector[i] << " ";
+	}
+
+	cout << "\n " << myVector.front()  << endl;
+
+
+	cout << "myVectorBack:" << endl;
+
+	for(unsigned int i =0; i<myVector.size(); i++){
+		cout << myVector[i] << " ";
+	}
+
+
+	//srting card = "KC QH 3D AH JH 2C 3S KS AS JS 3C KH AD QC JD QS 3H KD AC JC 2D 2H 2S QD ";
 
 	FigureCard c1 = FigureCard('H', 'Q');
 	cout << c1.toString() << endl;
@@ -13,6 +53,8 @@ int main(int argc, char **argv) {
 	cout << c2.toString() << endl;
 //	int argc, char **argv
 	 char* configurationFile = argv[1];
+
+
 
 	Game game = Game(configurationFile);
 	/*game.init();
